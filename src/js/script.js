ab267638256a0922b6338857807e321436dca8ce
Diogo <diogopelinsonduartemoraes@gmail.com>
2025-04-01 09:16:22 -0300

    Adicionando tipos de variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -18,4 +18,22 @@ let exemplo1={}; //Lista com objeto
 
 let exemplo2=[]; //Objeto Ray
 
+//TIPOS DE VARIAVEIS
+let exemplo3 = 10;
+console.log(typeof exemplo3)
+
+
+let exemplo4 = "Aula";
+console.log(typeof exemplo4)
+
+let exemplo5 = true;
+console.log(typeof exemplo5)
+
+let exemplo6 = ["Diogo", "Fiap", "JavaScript"]
+console.log(typeof exemplo6)
+
+//CONVERSÕES
+
+
+
 
