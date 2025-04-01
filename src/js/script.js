6d157eb4971aec1366041fd38b9f5cda201e644e
Diogo <diogopelinsonduartemoraes@gmail.com>
2025-04-01 09:10:05 -0300

    Declaração e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+//DECLARAÇÕES E VARIAVEIS
+var nome = "fiap";
+console.log(nome)
+
+let idade = 17;
+console.log(idade)  
+
+const sobrenome = "Pelinson";
+console.log(sobrenome)
+//undefined
+let aula;
+console.log(aula)
+//null
+let valor = null
+console.log(valor)
+
+let exemplo1={}; //Lista com objeto
+
+let exemplo2=[]; //Objeto Ray
+
+
