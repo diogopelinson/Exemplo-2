bab6862ed371411c6de0564b3c533f14d6787b8f
Diogo <diogopelinsonduartemoraes@gmail.com>
2025-04-01 09:27:34 -0300

    Adicionando converões

--- src/js/script.js
+++ src/js/script.js
@@ -20,19 +20,38 @@ let exemplo2=[]; //Objeto Ray
 
 //TIPOS DE VARIAVEIS
 let exemplo3 = 10;
-console.log(typeof exemplo3)
+console.log(typeof exemplo3);
 
 
 let exemplo4 = "Aula";
-console.log(typeof exemplo4)
+console.log(typeof exemplo4);
 
 let exemplo5 = true;
-console.log(typeof exemplo5)
+console.log(typeof exemplo5);
 
-let exemplo6 = ["Diogo", "Fiap", "JavaScript"]
-console.log(typeof exemplo6)
+let exemplo6 = ["Diogo", "Fiap", "JavaScript"];
+console.log(typeof exemplo6);
 
 //CONVERSÕES
+
+//float => inteiro
+let numFloat = 123.124;
+console.log(parseInt(numFloat));
+
+//string => float
+let numString = "547.987";
+console.log(parseFloat(numString));
+
+//float => string
+
+let numFloat1 = 552.19;
+console.log(numFloat1.toString());
+
+
+let numInt = 100;
+console.log(numInt.toString());
+
+
 
 
 
